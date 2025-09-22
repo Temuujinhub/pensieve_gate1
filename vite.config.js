748a1379bd549225483da515cd3a097d61eeeb91
unknown <stemuujin@gmail.com>
2025-09-22 15:22:50 +0800

    Fix GitHub Pages base path, build for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path';
 
 export default defineConfig({
+  base: '/pensieve_gate1/', // энэ repo-н нэр
   plugins: [react()],
   resolve: {
     alias: {
